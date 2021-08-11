bc3908c295989eadf5203979232c6c3ed251bc6a
Nilomiranda <nilomiranda3@gmail.com>
2021-08-11 20:57:15 -0300

    updated packages and scripts

---
+++ packages/client/.next/server/pages/index.js
@@ -0,0 +1,35 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+;(function () {
+  var exports = {}
+  exports.id = 'pages/index'
+  exports.ids = ['pages/index']
+  exports.modules = {
+    /***/ './pages/index.tsx':
+      /*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+      /***/ function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+        'use strict'
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "default": function() { return /* binding */ Index; }\n/* harmony export */ });\nfunction Index() {\n  return null;\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jbGllbnQvLi9wYWdlcy9pbmRleC50c3g/ZGI3NiJdLCJuYW1lcyI6WyJJbmRleCJdLCJtYXBwaW5ncyI6Ijs7OztBQUFlLFNBQVNBLEtBQVQsR0FBaUI7QUFDOUIsU0FBTyxJQUFQO0FBQ0QiLCJmaWxlIjoiLi9wYWdlcy9pbmRleC50c3guanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBJbmRleCgpIHtcbiAgcmV0dXJuIG51bGxcbn1cbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/index.tsx\n'
+        )
+
+        /***/
+      },
+  }
+  // load runtime
+  var __webpack_require__ = require('../webpack-runtime.js')
+  __webpack_require__.C(exports)
+  var __webpack_exec__ = function (moduleId) {
+    return __webpack_require__((__webpack_require__.s = moduleId))
+  }
+  var __webpack_exports__ = __webpack_exec__('./pages/index.tsx')
+  module.exports = __webpack_exports__
+})()
---
+++ packages/client/.next/static/chunks/pages/index.js
@@ -0,0 +1,50 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+;(self['webpackChunk_N_E'] = self['webpackChunk_N_E'] || []).push([
+  ['pages/index'],
+  {
+    /***/ './pages/index.tsx':
+      /*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+      /***/ function (module, __webpack_exports__, __webpack_require__) {
+        'use strict'
+        eval(
+          "__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* binding */ Index; }\n/* harmony export */ });\n/* module decorator */ module = __webpack_require__.hmd(module);\nfunction Index() {\n  return null;\n}\n_c = Index;\n\nvar _c;\n\n$RefreshReg$(_c, \"Index\");\n\n;\n    var _a, _b;\n    // Legacy CSS implementations will `eval` browser code in a Node.js context\n    // to extract CSS. For backwards compatibility, we need to check we're in a\n    // browser context before continuing.\n    if (typeof self !== 'undefined' &&\n        // AMP / No-JS mode does not inject these helpers:\n        '$RefreshHelpers$' in self) {\n        var currentExports = module.__proto__.exports;\n        var prevExports = (_b = (_a = module.hot.data) === null || _a === void 0 ? void 0 : _a.prevExports) !== null && _b !== void 0 ? _b : null;\n        // This cannot happen in MainTemplate because the exports mismatch between\n        // templating and execution.\n        self.$RefreshHelpers$.registerExportsForReactRefresh(currentExports, module.id);\n        // A module can be accepted automatically based on its exports, e.g. when\n        // it is a Refresh Boundary.\n        if (self.$RefreshHelpers$.isReactRefreshBoundary(currentExports)) {\n            // Save the previous exports on update so we can compare the boundary\n            // signatures.\n            module.hot.dispose(function (data) {\n                data.prevExports = currentExports;\n            });\n            // Unconditionally accept an update to this module, we'll check if it's\n            // still a Refresh Boundary later.\n            module.hot.accept();\n            // This field is set when the previous version of this module was a\n            // Refresh Boundary, letting us know we need to check for invalidation or\n            // enqueue an update.\n            if (prevExports !== null) {\n                // A boundary can become ineligible if its exports are incompatible\n                // with the previous exports.\n                //\n                // For example, if you add/remove/change exports, we'll want to\n                // re-execute the importing modules, and force those components to\n                // re-render. Similarly, if you convert a class component to a\n                // function, we want to invalidate the boundary.\n                if (self.$RefreshHelpers$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)) {\n                    module.hot.invalidate();\n                }\n                else {\n                    self.$RefreshHelpers$.scheduleUpdate();\n                }\n            }\n        }\n        else {\n            // Since we just executed the code for the module, it's possible that the\n            // new exports made it ineligible for being a boundary.\n            // We only care about the case when we were _previously_ a boundary,\n            // because we already accepted this update (accidental side effect).\n            var isNoLongerABoundary = prevExports !== null;\n            if (isNoLongerABoundary) {\n                module.hot.invalidate();\n            }\n        }\n    }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcGFnZXMvaW5kZXgudHN4P2RiNzYiXSwibmFtZXMiOlsiSW5kZXgiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQWUsU0FBU0EsS0FBVCxHQUFpQjtBQUM5QixTQUFPLElBQVA7QUFDRDtLQUZ1QkEsSyIsImZpbGUiOiIuL3BhZ2VzL2luZGV4LnRzeC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEluZGV4KCkge1xuICByZXR1cm4gbnVsbFxufVxuIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.tsx\n"
+        )
+
+        /***/
+      },
+
+    /***/ './node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdanilomiranda%2FDeveloper%2Fd-timevault%2Fpackages%2Fclient%2Fpages%2Findex.tsx!':
+      /*!********************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdanilomiranda%2FDeveloper%2Fd-timevault%2Fpackages%2Fclient%2Fpages%2Findex.tsx! ***!
+  \********************************************************************************************************************************************************************************************************/
+      /***/ function (__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+        eval(
+          '\n    (window.__NEXT_P = window.__NEXT_P || []).push([\n      "/",\n      function () {\n        return __webpack_require__(/*! ./pages/index.tsx */ "./pages/index.tsx");\n      }\n    ]);\n  //# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz9lNTMwIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFDQTtBQUNBO0FBQ0E7QUFDQSxlQUFlLG1CQUFPLENBQUMsNENBQTRFO0FBQ25HO0FBQ0EiLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvbmV4dC9kaXN0L2J1aWxkL3dlYnBhY2svbG9hZGVycy9uZXh0LWNsaWVudC1wYWdlcy1sb2FkZXIuanM/cGFnZT0lMkYmYWJzb2x1dGVQYWdlUGF0aD0lMkZVc2VycyUyRmRhbmlsb21pcmFuZGElMkZEZXZlbG9wZXIlMkZkLXRpbWV2YXVsdCUyRnBhY2thZ2VzJTJGY2xpZW50JTJGcGFnZXMlMkZpbmRleC50c3ghLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG4gICAgKHdpbmRvdy5fX05FWFRfUCA9IHdpbmRvdy5fX05FWFRfUCB8fCBbXSkucHVzaChbXG4gICAgICBcIi9cIixcbiAgICAgIGZ1bmN0aW9uICgpIHtcbiAgICAgICAgcmV0dXJuIHJlcXVpcmUoXCIvVXNlcnMvZGFuaWxvbWlyYW5kYS9EZXZlbG9wZXIvZC10aW1ldmF1bHQvcGFja2FnZXMvY2xpZW50L3BhZ2VzL2luZGV4LnRzeFwiKTtcbiAgICAgIH1cbiAgICBdKTtcbiAgIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdanilomiranda%2FDeveloper%2Fd-timevault%2Fpackages%2Fclient%2Fpages%2Findex.tsx!\n'
+        )
+
+        /***/
+      },
+  },
+  /******/ function (__webpack_require__) {
+    // webpackRuntimeModules
+    /******/ // runtime can't be in strict mode because a global variable is assign and maybe created.
+    /******/
+    /******/ var __webpack_exec__ = function (moduleId) {
+      return __webpack_require__((__webpack_require__.s = moduleId))
+    }
+    /******/ var __webpack_exports__ = __webpack_exec__(
+      './node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fdanilomiranda%2FDeveloper%2Fd-timevault%2Fpackages%2Fclient%2Fpages%2Findex.tsx!'
+    )
+    /******/ _N_E = __webpack_exports__
+    /******/
+  },
+])

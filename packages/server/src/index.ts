18e6c7b65487c834995e458d672a7537c26cb0b8
Nilomiranda <nilomiranda3@gmail.com>
2021-08-12 20:16:34 -0300

    create time capsule schema, loaded and connected prisma client

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,9 @@ import Koa from 'koa'
 import dotenv from 'dotenv'
 import router from './routes'
 import bodyParser from 'koa-bodyparser'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const app = new Koa()
 
 dotenv.config()
@@ -9,6 +12,14 @@ dotenv.config()
 app.listen(process.env.PORT, () => {
   console.log(`Application running on port ${process.env.PORT}`)
 })
+
+prisma?.$connect().then(() => {
+  console.log("Successfully connected to prisma client")
+}).catch(err => {
+  console.error("Error trying to connect to prisma client", err)
+})
+
+app.context.prisma = prisma
 
 app.use(bodyParser())
 app.use(router.routes())

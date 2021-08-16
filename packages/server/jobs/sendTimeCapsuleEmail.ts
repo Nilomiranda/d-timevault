aab093fe76956ca72d8bbe1781a448c1d968308b
Nilomiranda <nilomiranda3@gmail.com>
2021-08-16 20:29:39 -0300

    marking time capsule as sent

--- packages/server/jobs/sendTimeCapsuleEmail.ts
+++ packages/server/jobs/sendTimeCapsuleEmail.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import {sendEmailWithTemplate} from "../src/services/mailjet";
 import { parentPort } from "worker_threads";
+import {isToday, parseISO} from "date-fns";
 
 const prisma = new PrismaClient();
 
@@ -15,9 +16,13 @@ const TIME_CAPSULE_EMAIL = 3106993;
       }
     }
   }).then(async confirmedTimeCapsules => {
-    const emailsList = confirmedTimeCapsules?.map(confirmedTimeCapsule => confirmedTimeCapsule?.email);
+    const timeCapsulesToSend = confirmedTimeCapsules
+      ?.filter(confirmedTimeCapsule => {
+        return isToday(confirmedTimeCapsule?.scheduledTo)
+      })
 
-    const emailsPromises = confirmedTimeCapsules?.map(confirmedTimeCapsule => {
+    const emailsPromises = timeCapsulesToSend
+      ?.map(confirmedTimeCapsule => {
       return sendEmailWithTemplate(
         TIME_CAPSULE_EMAIL,
         [{ Email: confirmedTimeCapsule?.email, Name: confirmedTimeCapsule?.email }],
@@ -27,6 +32,16 @@ const TIME_CAPSULE_EMAIL = 3106993;
 
     try {
       await Promise.all(emailsPromises)
+      await prisma?.timeCapsule?.updateMany({
+        where: {
+          id: {
+            in: timeCapsulesToSend?.map(timeCapsulesToSend => timeCapsulesToSend?.id),
+          }
+        },
+        data: {
+          emailSent: true,
+        }
+      })
     } catch (err) {
       console.error('Error sending emails containing time capsules', err)
     } finally {
